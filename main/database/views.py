f3859c4163511568b0f92d7f9cf8748e6763d0fa
jysloth <cjyclx@gmail.com>
2020-10-10 20:36:03 +0800

    (update) minor change

--- main/database/views.py
+++ main/database/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer, BugSerializer,RoleSerializer, CommentSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 class UserViewSet(viewsets.ViewSet):
     """
@@ -11,13 +12,13 @@ class UserViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = User.objects.all()
         serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
 
     def retrieve(self, request, pk=None):
         queryset = User.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = UserSerializer(user)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
 
 
 
@@ -29,23 +30,23 @@ class RoleViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Role.objects.all()
         serializer = RoleSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
 
     def retrieve(self, request, pk=None):
         queryset = Role.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = RoleSerializer(user)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
 
 
 class BugViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Bug.objects.all()
         serializer = BugSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
 
     def retrieve(self, request, pk=None):
         queryset = Bug.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = BugSerializer(user)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
